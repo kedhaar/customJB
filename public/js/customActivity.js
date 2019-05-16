bb77687c20708b1e04f585831fc611c41d4a8f03
SaikatChanda <50450705+SaikatChanda@users.noreply.github.com>
2019-05-16 15:49:00 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -35,6 +35,17 @@ define([
 
     function initialize(data) {
         console.log('On initialize');
+        
+        var request = require('request');
+        var url = "https://endhczbqshdnr.x.pipedream.net/log= On initialize";
+        
+        request(url, function (error, response, body) {
+            if (!error && response.statusCode == 200) {
+             console.log(body);
+             }
+        });
+        
+        
         console.log(data);
         if (data) {
             payload = data;
