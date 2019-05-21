7f739a75ba5cd0981c978ce551b51d216abfa506
SaikatChanda <50450705+SaikatChanda@users.noreply.github.com>
2019-05-21 18:14:34 +0530

    Update customActivity.js

--- public/js/customActivity.js
+++ public/js/customActivity.js
@@ -1,144 +1,162 @@
 define([
-    'postmonger'
-], function (
-    Postmonger
+ 'postmonger'
+], function(
+ Postmonger
 ) {
-    'use strict';
+ 'use strict';
 
-    var connection = new Postmonger.Session();
-    var authTokens = {};
-    var payload = {};
-    
-    var steps = [
-        { "label": "Configure WebHook", "Key": "step1"}
-    ];
-    
-    var currentStep = steps[0].key;
-    
-    $(window).ready(onRender);
+ var connection = new Postmonger.Session();
+ var authTokens = {};
+ var payload = {};
+ var deFieldsKey = [];
 
-    connection.on('initActivity', initialize);
-    connection.on('requestedTokens', onGetTokens);
-    connection.on('requestedEndpoints', onGetEndpoints);
+ var steps = [{
+  "label": "Configure WebHook",
+  "Key": "step1"
+ }];
 
-    connection.on('clickedNext', save);
-   
-    function onRender() {
-        console.log('On render');
-        // JB will respond the first time 'ready' is called with 'initActivity'
-        connection.trigger('ready');
+ var currentStep = steps[0].key;
 
-        connection.trigger('requestTokens');
-        connection.trigger('requestEndpoints');
-	    
-	var deDefKey;
-	connection.trigger('requestSchema');
-        connection.on('requestedSchema', function (data) {
-   	// save schema
-	deDefKey = data['schema'];
-   	console.log('*** Schema ***', deDefKey);
-	console.log('*** key elements ***', deDefKey.length);
-	console.log('*** key elements ***', deDefKey[0].key);
-	});
-	    
-	    
-	var eventDefinitionKey;
-	connection.trigger('requestTriggerEventDefinition');
-	
-	connection.on('requestedTriggerEventDefinition',
-	function(eventDefinitionModel) {
-    	if(eventDefinitionModel){
+ $(window).ready(onRender);
 
-       	 eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
-       	 console.log(">>>Event Definition Key " + eventDefinitionKey);
-        /*If you want to see all*/
-        console.log('>>>Request Trigger', 
-        JSON.stringify(eventDefinitionModel));
+ connection.on('initActivity', initialize);
+ connection.on('requestedTokens', onGetTokens);
+ connection.on('requestedEndpoints', onGetEndpoints);
+
+ connection.on('clickedNext', save);
+
+ function onRender() {
+  console.log('On render');
+  // JB will respond the first time 'ready' is called with 'initActivity'
+  connection.trigger('ready');
+
+  connection.trigger('requestTokens');
+  connection.trigger('requestEndpoints');
+
+  var deDefKey;
+  connection.trigger('requestSchema');
+  connection.on('requestedSchema', function(data) {
+   // save schema
+   deDefKey = data['schema'];
+	   for (var i = 0; i < deDefKey.length; i++) {
+	    var obj = deDefKey[0].key;
+	    deFieldsKey.push(obj);
+	   }
+   console.log('*** Schema ***', deDefKey);
+   console.log('*** key elements ***', deDefKey.length);
+   console.log('*** key elements ***', deDefKey[0].key);
+  });
+
+
+  var eventDefinitionKey;
+  connection.trigger('requestTriggerEventDefinition');
+
+  connection.on('requestedTriggerEventDefinition',
+   function(eventDefinitionModel) {
+    if (eventDefinitionModel) {
+
+     eventDefinitionKey = eventDefinitionModel.eventDefinitionKey;
+     console.log(">>>Event Definition Key " + eventDefinitionKey);
+     /*If you want to see all*/
+     console.log('>>>Request Trigger',
+      JSON.stringify(eventDefinitionModel));
     }
 
-});
+   });
 
-    }
+ }
 
-    function initialize(data) {
-        console.log('On initialize');
-        
-        console.log(data);
-        if (data) {
-            payload = data;
-        }
-        
-        var hasInArguments = Boolean(
-            payload['arguments'] &&
-            payload['arguments'].execute &&
-            payload['arguments'].execute.inArguments &&
-            payload['arguments'].execute.inArguments.length > 0
-        );
-        console.log('hasinArguments--->' + hasInArguments);
-        /*var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
+ function initialize(data) {
+  console.log('On initialize');
 
-        console.log(inArguments);
+  console.log(data);
+  if (data) {
+   payload = data;
+  }
 
-        $.each(inArguments, function (index, inArgument) {
-            $.each(inArgument, function (key, val) {
-                
-                console.log(in each block);
-            });
-        });*/
-        
-         if (hasInArguments && payload['arguments'].execute.inArguments[0].webHookURl) {
-			$('#inputTextBox').val(payload['arguments'].execute.inArguments[0].webHookURl);
-           } 
-        
-        connection.trigger('updateButton', {
-            button: 'next',
-            text: 'done',
-            visible: true
-        });
-    }
+  var hasInArguments = Boolean(
+   payload['arguments'] &&
+   payload['arguments'].execute &&
+   payload['arguments'].execute.inArguments &&
+   payload['arguments'].execute.inArguments.length > 0
+  );
+  console.log('hasinArguments--->' + hasInArguments);
+  /*var inArguments = hasInArguments ? payload['arguments'].execute.inArguments : {};
 
-    function onGetTokens(tokens) {
-        console.log(tokens);
-        authTokens = tokens;
-    }
+  console.log(inArguments);
 
-    function onGetEndpoints(endpoints) {
-        console.log(endpoints);
-    }
+  $.each(inArguments, function (index, inArgument) {
+      $.each(inArgument, function (key, val) {
+          
+          console.log(in each block);
+      });
+  });*/
 
-    function save() {
-        $('#errorMsgPreview').text('');
-        console.log('customActivity.js ----->  clicked on save');
-        
-        var webHookURlValue = $('#inputTextBox').val();
-        var errMsg;
-        
-        console.log(webHookURlValue);
-      
-	    var hasInArguments = Boolean( payload['arguments'] &&  payload['arguments'].execute &&  payload['arguments'].execute.inArguments && 
-	    payload['arguments'].execute.inArguments.length > 0);
+  if (hasInArguments && payload['arguments'].execute.inArguments[0].webHookURl) {
+   $('#inputTextBox').val(payload['arguments'].execute.inArguments[0].webHookURl);
+  }
 
-        if (true) {
-	    //    payload['arguments'].execute.inArguments[0].inputTextBox = webHookURlValue;
-	    payload['arguments'].execute.inArguments = [{
-            "webHookURl": webHookURlValue,
-            "tokens": authTokens
-        }];
-	    }
-        if (webHookURlValue.length == 0) { 
-		
-	        errMsg = 'Error: WebHook URl cannot be blank.'; 
-	        console.error(errMsg);
-	        $('#errorMsgConfig').text(errMsg); 
-		$('#errorMsgConfig').show();
-	        connection.trigger('ready');
-	    }else {
-	    console.log('Updated Connection Activity');
-	    console.log('payload------>'+ JSON.stringify(payload));
-	    payload['metaData'].isConfigured = true; 
-	    connection.trigger('updateActivity', payload);
-	    }
-        /*payload['arguments'].execute.inArguments = [{
+  connection.trigger('updateButton', {
+   button: 'next',
+   text: 'done',
+   visible: true
+  });
+ }
+
+ function onGetTokens(tokens) {
+  console.log(tokens);
+  authTokens = tokens;
+ }
+
+ function onGetEndpoints(endpoints) {
+  console.log(endpoints);
+ }
+
+ function save() {
+  $('#errorMsgPreview').text('');
+  console.log('customActivity.js ----->  clicked on save');
+
+  var webHookURlValue = $('#inputTextBox').val();
+  var errMsg;
+
+  console.log(webHookURlValue);
+
+  var hasInArguments = Boolean(payload['arguments'] && payload['arguments'].execute && payload['arguments'].execute.inArguments &&
+   payload['arguments'].execute.inArguments.length > 0);
+
+  if (true) {
+   //    payload['arguments'].execute.inArguments[0].inputTextBox = webHookURlValue;
+   payload['arguments'].execute.inArguments = [{
+    "webHookURl": webHookURlValue,
+    "tokens": authTokens
+   }];
+	  payload['arguments'].execute.inArguments = deFieldsKey;
+	  console.log('Final inargs pay load' + payload['arguments'].execute.inArguments);
+	  
+  }
+  if (webHookURlValue.length == 0) {
+
+   errMsg = 'Error: WebHook URl cannot be blank.';
+   console.error(errMsg);
+   $('#errorMsgConfig').text(errMsg);
+   $('#errorMsgConfig').show();
+   connection.trigger('ready');
+  } else {
+   console.log('Updated Connection Activity');
+   console.log('payload------>' + JSON.stringify(payload));
+   payload['metaData'].isConfigured = true;
+   connection.trigger('updateActivity', payload);
+  }
+  /*payload['arguments'].execute.inArguments = [{
+      "webHookURl": webHookURlValue,
+      "tokens": authTokens,
+      "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}",
+      "firstName": "{{Contact.Attribute.CustomActivity.FirstName}}",
+      "lastName": "{{Contact.Attribute.CustomActivity.LastName}}",
+      "city": "{{Contact.Attribute.CustomActivity.City}}",
+      "country": "{{Contact.Attribute.CustomActivity.Country}}"
+  }];
+  /*payload['arguments'].execute.inArguments = [{
             "webHookURl": webHookURlValue,
             "tokens": authTokens,
             "emailAddress": "{{Contact.Attribute.PostcardJourney.EmailAddress}}",
@@ -152,6 +170,10 @@ define([
 
         console.log(payload);
         connection.trigger('updateActivity', payload);*/
-    }
+  payload['metaData'].isConfigured = true;
+
+  console.log(payload);
+  connection.trigger('updateActivity', payload);*/
+ }
 
 });
